ab844b5a874b3177c7acef0a888929634c05ad37
Daniel van der Woude <c.d.a.vanderwoude@gmail.com>
2016-10-14 11:00:56 +0200

    parsed customer data

--- pensionapp/app.js
+++ pensionapp/app.js
@@ -4,6 +4,6 @@ const fs = require('fs');
 // Read the customer data json
 fs.readFile(__dirname + '/customer.json', 'utf-8', (err, data) => {
 	// Prase the file to a readable object
-	let parsedData = JSON.parse(data) 
-	console.log(parsedData)
-} )
+	let parsedData = JSON.parse(data); 
+	console.log(parsedData);
+} );
