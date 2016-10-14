4fedd22ce04378f4c43976d33a60f6439569c4ca
Daniel van der Woude <c.d.a.vanderwoude@gmail.com>
2016-10-14 17:31:02 +0200

    finished part 2

--- pensionapp/app.js
+++ pensionapp/app.js
@@ -3,47 +3,48 @@ let pensionReader = require(__dirname + '/json-file-reader');
 
 pensionReader(__dirname + '/customers.json', (customer) => {
 
-	console.log(customer)
+	for (var x = customer.length - 1; x >= 0; x--) {
 
-	// // Do the interest math
-	// for (var i = customer.pension.duration - 1; i >= 0; i--) {
-	// 	// Add monthly spend to all the scenarios		
-	// 	customer.pension.endamount.pessimistic += (customer.finances.monthlyadd * 12);
-	// 	customer.pension.endamount.average 	   += (customer.finances.monthlyadd * 12);
-	// 	customer.pension.endamount.optimistic  += (customer.finances.monthlyadd * 12);
+		for (var i = customer[x].pension.duration - 1; i >= 0; i--) {
+		
+				// Add monthly spend to all the scenarios		
+				customer[x].pension.endamount.pessimistic += (customer[x].finances.monthlyadd * 12);
+				customer[x].pension.endamount.average += (customer[x].finances.monthlyadd * 12);
+				customer[x].pension.endamount.optimistic += (customer[x].finances.monthlyadd * 12);
 
-	// 	// Calculate the added interest
-	// 	customer.pension.endamount.pessimistic *= (customer.pension.interest.pessimistic);
-	// 	customer.pension.endamount.average     *= (customer.pension.interest.average);
-	// 	customer.pension.endamount.optimistic  *= (customer.pension.interest.optimistic);
-	// };
+				// Calculate the added interest
+				customer[x].pension.endamount.pessimistic *= (customer[x].pension.interest.pessimistic);
+				customer[x].pension.endamount.average *= (customer[x].pension.interest.average);
+				customer[x].pension.endamount.optimistic *= (customer[x].pension.interest.optimistic);
+		}// end of pension duration loop 
 
-	// // Output our data
-	// console.log("Welcome " + customer.name + " to our advanced pension planner!");
-	// console.log("You are starting with €" + customer.finances.startcapital + " and add a monthly amount of €" + customer.finances.monthlyadd);
-	// console.log("When you retire at age " + customer.pension.age + " you will have the following:");
-	// // Scenario outcomes 
-	// console.log("In a pessimistic scenario you will earn: €" + prettyNr(customer.pension.endamount.pessimistic));
-	// console.log("In an average scenario you will earn: €" + prettyNr(customer.pension.endamount.average));
-	// console.log("In an optimistic scenario you will earn: €" + prettyNr(customer.pension.endamount.optimistic));
-
+	// Output our data
+		console.log("Welcome " + customer[x].name + " to our advanced pension planner!");
+		console.log("You are starting with €" + customer[x].finances.startcapital + " and add a monthly amount of €" + customer[x].finances.monthlyadd);
+		console.log("When you retire at age " + customer[x].pension.age + " you will have the following:");
+	// Scenario outcomes 
+		console.log("In a pessimistic scenario you will earn: €" + prettyNr(customer[x].pension.endamount.pessimistic));
+		console.log("In an average scenario you will earn: €" + prettyNr(customer[x].pension.endamount.average));
+		console.log("In an optimistic scenario you will earn: €" + prettyNr(customer[x].pension.endamount.optimistic)+ "\n");
 	}
-);
+});//end of pension reader 
 
-// // Making the numbers in the calculation pretty 
 
-// // Round numbers in decimals
-// let roundDecimal = (number) => {
-// 	return Math.round((number * 100) / 100);
-// };
 
-// // Add Comma's to numbers
-// let addCommas = (number) => {
-// 	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-// };
+// Round numbers in decimals
+let roundDecimal = (number) => {
+	return Math.round((number * 100) / 100);
+};
 
-// // Add Comma's and round decimals
-// let prettyNr = (number) => {
-// 	return addCommas(roundDecimal(number));
-// };
+// Add Comma's to numbers
+let addCommas = (number) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
+// Add Comma's and round decimals
+let prettyNr = (number) => {
+	return addCommas(roundDecimal(number));
+};
+		
+
 
--- pensionapp/json-file-reader.js
+++ pensionapp/json-file-reader.js
@@ -9,26 +9,29 @@ function jsonReader (filename, callback) {
 		// parse the json file 
 		let customer = JSON.parse(data);
 
+		//loop though all the customer profiles
 		for (var i = customer.length - 1; i >= 0; i--) {
 
-		// Set end amount prop and calucate total duration
+			// Set end amount prop and calucate total duration
 			customer[i].pension.endamount = {
 				pessimistic: customer[i].finances.startcapital,
 				average: customer[i].finances.startcapital,
 				optimistic: customer[i].finances.startcapital
 			};
 			customer[i].pension.duration = (customer[i].pension.age - customer[i].age);
-		}	
-		
-		 // end 1st for loop
 
-		// callback json
-		callback(customer);
-	})
-} 
+		}//end of customer profile loop 
+
+	// callback json
+	callback(customer);
+
+	}) // end of readfile function
+} // end of jsonreader function
+
 
 // Exports the filereader function
 module.exports = jsonReader
+
 
 
 
