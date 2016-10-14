f6b6f13544182498ce23cf83c5c26febe8d54f76
Daniel van der Woude <c.d.a.vanderwoude@gmail.com>
2016-10-14 11:13:00 +0200

    parsed customer data

--- pensionapp/app.js
+++ pensionapp/app.js
@@ -5,5 +5,11 @@ const fs = require('fs');
 fs.readFile(__dirname + '/customer.json', 'utf-8', (err, data) => {
 	// Prase the file to a readable object
 	let parsedData = JSON.parse(data); 
-	console.log(parsedData);
+	calcCompound(parsedData)
 } );
+
+// Function to caculate compound interest from a customer object 
+var calcCompound = (customer) => {
+	console.log(customer)
+	customer.pension.endamount = 0
+}
